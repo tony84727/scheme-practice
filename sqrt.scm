78d95adde05f0d73237c1844c4725c9ce692b4b1
Tony Duan <tony84727@gmail.com>
2021-02-19 22:45:09 +0800

    nest sqrt helper functions

--- sqrt.scm
+++ sqrt.scm
@@ -7,16 +7,14 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (improve guess x)
-  (average guess (/ x guess)))
 
-(define (sqrt-iter guess x)
+(define (sqrt x)
+  (define (sqrt-iter guess x)
   (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x) x)))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (sqrt x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
   (sqrt-iter 1 x))
