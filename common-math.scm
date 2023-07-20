2575b0652f2c97630f544ff2ec1b67568ad7f75d
Tony Duan <tony84727@gmail.com>
2023-07-20 22:44:07 +0800

    gcd

--- common-math.scm
+++ common-math.scm
@@ -7,3 +7,18 @@
 
 (define (average x y)
   (/ (+ x y) 2))
+
+(define (slow-gcd a b)
+  (if (or (= a 0) (= b 0))
+    (+ a b)
+    (if (> b a)
+      (slow-gcd a (- b a))
+      (slow-gcd (- a b) b))))
+
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+    
+(define (gcd-3 a b c)
+  (gcd (gcd a b) c))
